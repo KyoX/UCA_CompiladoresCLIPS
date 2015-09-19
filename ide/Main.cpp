a4cdd8b8f9a21451cd4dfe78bba514c63538598e
KyoX <njserranor@gmail.com>
2015-09-18 22:38:31 -0600

    Creacion inicial del repositorio
    - Pantalla grafica principal
    - incorporacion de referencia externa con archivo de test.cpp

---
+++ ide/Main.cpp
@@ -0,0 +1,11 @@
+#include "Main.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThread]
+void main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	ide::Main form;
+	Application::Run(%form);
+}
